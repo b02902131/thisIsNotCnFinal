90c071bf7a8a7dfe3058c125a63b76f443ab47ca
Bigyo <Bigyo@BigyodeMacBook-Pro.local>
2016-01-17 17:45:14 +0800

    state(0,0)

--- server.c
+++ server.c
@@ -59,6 +59,13 @@ server svr;  // server
 request* requestP = NULL;  // point to a list of requests
 int maxfd;  // size of open file descriptor table, size of request list
 
+
+// my function
+static void sendUI(int conn_fd, char * ui);
+static void changeStateAndSendUI(int conn_fd, int state, int substate, char * ui);
+// my function
+
+
 static void init_server(unsigned short port);
 // initailize a server, exit for error
 
@@ -79,7 +86,67 @@ static int handle_read(request* reqP);
 // error code:
 // -1: client connection error
 
-char *login_table = "\n======================== Welcome to BigyoChat ========================\n1.Login\n2.Create new account\n===================================================================\nMy selection:\n";
+char *select_err = "\n------------------------------ Error ------------------------------\n*Warning:Your selection is error!!!\n=> Please select again!!!\n";
+char *login_table = "\n======================== Welcome to CnChat ========================\n1.Login\n2.Create new account\n===================================================================\nMy selection:\n";
+
+char *login_account = "\n------------------------------ Login ------------------------------\n1.Account:\n";
+char *login_pwd = "2.Passward:\n";
+char *login_account_empty = "\n------------------------------ Error ------------------------------\n*Warning:You did not enter any account!!!\n=> Please enter again!!!\n";
+char *login_online = "\n------------------------------ Error ------------------------------\n*Warning:You have login,you cannot login twice!!!\n";
+char *login_error = "\n------------------------------ Error ------------------------------\n*Warning:Your account and passward is error!!!\n=> Please try it again!!!\n";
+char *login_error1 = "\n*Warning:You have three chance!!!\n=> Error:1\n";
+char *login_error2 = "\n*Warning:You have three chance!!!\n=> Error:2\n";
+char *login_error3 = "\n*Warning:You have three chance!!!\n=> Error:3\n";
+char *login_error4 = "\n*Warning:You have error more than three times!!!\n=> Please try it again!!!\n";
+
+//Register
+char *register_account = "\n------------------------ Create New Account -----------------------\nWarning:All should shorter than 10 words!\n1.Account:\n";
+char *register_pwd = "2.Passward:\n";
+
+char *register_account_empty = "\n------------------------------ Error ------------------------------\n*Warning:You did not enter any account!!!\n=> Please create again!!!\n";
+char *register_account_long = "\n------------------------------ Error ------------------------------\n*Warning:Your account is longer than 10 words!!!\n=> Please create again!!!\n";
+char *register_account_used = "\n------------------------------ Error ------------------------------\n*Warning:This account has been used!!!\n=> Please create again!!!\n";
+
+char *register_pwd_empty = "\n------------------------------ Error ------------------------------\n*Warning:You did not enter any passward!!!\n=> Please create again!!!\n";
+char *register_pwd_long = "\n------------------------------ Error ------------------------------\n*Warning:Your passward is longer than 10 words!!!\n=> Please create again!!!\n";
+
+//Main
+char *main_title = "\n======================== Welcome to CnChat ========================\n";
+char *main_friend_title1 = "\n------------------------------ Friend -----------------------------\n";
+char *main_friend_title2 = "     Online            Chat                ID             Account\n";
+char *main_friend_title3 = "-------------------------------------------------------------------\n";
+char *main_menu = "-------------------------------------------------------------------\nPlease select the service that you want?\n1.Chat Room\n2.File Transfer\n3.Historical Message\n4.Offline Message\n5.Exit\n-------------------------------------------------------------------\nMy selection:\n";
+char *main_exit = "Good bye~\n";
+
+//History
+char *history_title = "\n------------------------ Historical Message ------------------------\n";
+char *history_end = "--------------------------------------------------------------------\nBack to home,press:/Home\n";
+
+//Offline
+char *offline_title = "\n------------------------- Offline Message --------------------------\n";
+char *offline_end = "--------------------------------------------------------------------\nBack to home,press:/Home\n";
+
+//Chat Room
+char *chat_title = "\n---------------------------- Chat Room -----------------------------\n";
+
+//File transfer
+char *file_tran_title = "\n-------------------------- File Transfer ---------------------------\nPlease select the service that you want?\n1.Send the file\n2.Receive the file\n--------------------------------------------------------------------\nMy selection:\n";
+char *file_title1 = "\n------------------------------ Friend -----------------------------\n";
+char *file_title2 = "     Online           Ready                ID             Account\n-------------------------------------------------------------------\n";
+char *file_title3 = "-------------------------------------------------------------------\nWarning:You can only send to those,who is online and ready!!\n=> Back to home,press:/Home\nPlease enter the friend's ID that you want to send?\nID:\n";
+char *file_err1 = "\n------------------------------ Error ------------------------------\n*Warning:You can not select those who is not online!!!\n=> Please select again!!!\n";
+char *file_err2 = "\n------------------------------ Error ------------------------------\n*Warning:You can not select those who is not ready!!!\n=> Please select again!!!\n";
+char *file_err3 = "\n------------------------------ Error ------------------------------\n*Warning:The ID that you entered is wrong!!!\n=> Please select again!!!\n";
+char *file_err4 = "\n------------------------------ Error ------------------------------\n*Warning:You do not enter any file name!!!\n=> Please enter again!!!\n";
+
+//Sending
+char *sending_title1 = "\n------------------------------ Sending ----------------------------\n1.Receiver:";
+char *sending_title2 = "\n2.Files(At most 5 files)\n";
+char *sending_err1 = "\n------------------------------ Error ------------------------------\n*Warning:You can not send more than 5 files at the same time!!!\n=> Please enter again!!!\n";
+char *sending_finish = "\n=> Back to home,press:/Home\n";
+
+//Receiving
+char *receiving_title = "\n----------------------------- Receiving ---------------------------\nBack to home,press:/Home\n";
 
 
 int main(int argc,char *argv[]){
@@ -251,7 +318,7 @@ int main(int argc,char *argv[]){
 
                         //state:8 substate:2(File Transfer-Sending finish)=============================
                         if(s1 == 8 && s2 == 2){
-                            
+
                         }//end state:8 substate:2(File Transfer-Sending finish)=============================
 
                         //state:8 substate:1(File Transfer-Receiving)==================================
@@ -329,7 +396,20 @@ int main(int argc,char *argv[]){
                         //state:0 substate:0(login table)==============================================
                         else if(s1 == 0 && s2 == 0)
                         {
-                            
+                            int login_table_input = atoi(requestP[conn_fd].buf);
+
+                            if(login_table_input != 1 && login_table_input !=2){
+                                changeStateAndSendUI(conn_fd, 0, 0, select_err);
+                                sendUI(conn_fd, login_table);   
+                            }
+                            else if(login_table_input == 1){
+                                requestP[conn_fd].login_error = 0;
+                                changeStateAndSendUI(conn_fd,1,1,login_account);
+                            }
+                            else if(login_table_input == 2){
+                                changeStateAndSendUI(conn_fd,2,1,register_account);
+                            }
+                            break;
                         }//(End)state:0 substate:0(login table)========================================
         			}//end else(i == svr.listen_fd)
         		}
@@ -341,7 +421,23 @@ int main(int argc,char *argv[]){
 }
 
 
-//===================================================================================================//
+//my function ============================================================================================//
+
+static void sendUI(int conn_fd, char * ui){
+    char buf[512];
+    sprintf(buf,"%s",ui);
+    write(requestP[conn_fd].conn_fd, buf, strlen(buf));
+}
+
+static void changeStateAndSendUI(int conn_fd, int state, int substate, char * ui){
+    requestP[conn_fd].state = state;
+    requestP[conn_fd].substate = substate;
+    char buf[512];
+    sprintf(buf,"%s",ui);
+    write(requestP[conn_fd].conn_fd, buf, strlen(buf));
+}
+
+//my function ============================================================================================//
 
 #include <fcntl.h>
 
@@ -378,7 +474,7 @@ static int handle_read(request* reqP)
     char buf[512];
     
     // Read in request from client
-    r = read(reqP->conn_fd, buf, sizeof(buf));
+    r = read(reqP->conn_fd, buf , sizeof(buf));
     if (r < 0) return -1;
     if (r == 0) return 0;
     char* p1 = strstr(buf, "\015\012");
