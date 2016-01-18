8f7bb0242e559f9946facb3a2e391ae2d0df190c
b01902137 <b01902137@linux7.csie.ntu.edu.tw>
2016-01-18 16:34:06 +0800

    title.c

--- title.c
+++ title.c
@@ -1,13 +1,11 @@
-
-
 char *select_err = "\n------------------------------ Error ------------------------------\n*Warning:Your selection is error!!!\n=> Please select again!!!\n";
-char *login_table = "\n======================== Welcome to CnChat ========================\n1.Login\n2.Create new account\n===================================================================\nMy selection:\n";
+char *login_table = "\n==================== Welcome to group 6 for cn ====================\nPlease input the number you want to do\n1.Login\n2.Create new account\n===================================================================\nMy selection:\n";
 
-char *login_account = "\n------------------------------ Login ------------------------------\n1.Account:\n";
+char *login_account = "------------------------------ Login ------------------------------\n1.Account:\n";
 char *login_pwd = "2.Passward:\n";
 char *login_account_empty = "\n------------------------------ Error ------------------------------\n*Warning:You did not enter any account!!!\n=> Please enter again!!!\n";
 char *login_online = "\n------------------------------ Error ------------------------------\n*Warning:You have login,you cannot login twice!!!\n";
-char *login_error = "\n------------------------------ Error ------------------------------\n*Warning:Wrong Passward or Account!!!\n";	//bigyo edited
+char *login_error = "\n------------------------------ Error ------------------------------\n*Warning:Your account and passward is error!!!\n=> Please try it again!!!\n";
 
 //Register
 char *register_account = "\n------------------------ Create New Account -----------------------\nWarning:All should shorter than 10 words!\n1.Account:\n";
@@ -21,39 +19,39 @@ char *register_pwd_empty = "\n------------------------------ Error -------------
 char *register_pwd_long = "\n------------------------------ Error ------------------------------\n*Warning:Your passward is longer than 10 words!!!\n=> Please create again!!!\n";
 
 //Main
-char *main_title = "\n======================== Welcome to CnChat ========================\n";
-char *main_friend_title1 = "\n------------------------------ Friend -----------------------------\n";
+char *main_title = "\n==================== Welcome to group 6 for cn ====================\n";
+char *main_friend_title1 = "\n--------------------------- User's lists --------------------------\n";
 char *main_friend_title2 = "     Online            Chat                ID             Account\n";
 char *main_friend_title3 = "-------------------------------------------------------------------\n";
-char *main_menu = "-------------------------------------------------------------------\nPlease select the service that you want?\n1.Chat Room\n2.File Transfer\n3.Historical Message\n4.Offline Message\n5.Exit\n-------------------------------------------------------------------\nMy selection:\n";
+char *main_menu = "-------------------------------------------------------------------\nPlease input the number you want to do\n1.Chat Room\n2.Send/Receive files\n3.Historical Messages\n4.Off-line Messages\n5.Exit\n-------------------------------------------------------------------\nMy selection:\n";
 char *main_exit = "Good bye~\n";
 
 //History
-char *history_title = "\n------------------------ Historical Message ------------------------\n";
-char *history_end = "--------------------------------------------------------------------\nBack to home,press:/Home\n";
+char *history_title = "\n------------------------ Historical Messages -----------------------\n";
+char *history_end = "--------------------------------------------------------------------\nPlease input '/Home' if you want back";
 
 //Offline
-char *offline_title = "\n------------------------- Offline Message --------------------------\n";
-char *offline_end = "--------------------------------------------------------------------\nBack to home,press:/Home\n";
+char *offline_title = "\n------------------------ Off-line Messages -------------------------\n";
+char *offline_end = "--------------------------------------------------------------------\nPlease input '/Home' if you want back";
 
 //Chat Room
 char *chat_title = "\n---------------------------- Chat Room -----------------------------\n";
 
 //File transfer
-char *file_tran_title = "\n-------------------------- File Transfer ---------------------------\nPlease select the service that you want?\n1.Send the file\n2.Receive the file\n--------------------------------------------------------------------\nMy selection:\n";
-char *file_title1 = "\n------------------------------ Friend -----------------------------\n";
+char *file_tran_title = "\n------------------------ Send/Receive files ------------------------\nPlease input the number you want to do\n1.Send the file\n2.Receive the file\n--------------------------------------------------------------------\nMy selection:\n";
+char *file_title1 = "\n--------------------------- User's lists --------------------------\n";
 char *file_title2 = "     Online           Ready                ID             Account\n-------------------------------------------------------------------\n";
-char *file_title3 = "-------------------------------------------------------------------\nWarning:You can only send to those,who is online and ready!!\n=> Back to home,press:/Home\nPlease enter the friend's ID that you want to send?\nID:\n";
-char *file_err1 = "\n------------------------------ Error ------------------------------\n*Warning:You can not select those who is not online!!!\n=> Please select again!!!\n";
-char *file_err2 = "\n------------------------------ Error ------------------------------\n*Warning:You can not select those who is not ready!!!\n=> Please select again!!!\n";
-char *file_err3 = "\n------------------------------ Error ------------------------------\n*Warning:The ID that you entered is wrong!!!\n=> Please select again!!!\n";
-char *file_err4 = "\n------------------------------ Error ------------------------------\n*Warning:You do not enter any file name!!!\n=> Please enter again!!!\n";
+char *file_title3 = "-------------------------------------------------------------------\n'You can only send to those who are online and ready!!'\n=> Please input '/Home' if you want back\nPlease input the ID of user's lists that you want to send\nID:\n";
+char *file_err1 = "\n------------------------------ Error ------------------------------\n'Your input ID is not online!!!'\n=> Please select again!!!\n";
+char *file_err2 = "\n------------------------------ Error ------------------------------\n'Your input ID is not ready!!!'\n=> Please select again!!!\n";
+char *file_err3 = "\n------------------------------ Error ------------------------------\n'The ID you inputed is wrong!!!'\n=> Please select again!!!\n";
+char *file_err4 = "\n------------------------------ Error ------------------------------\n'You do not enter any file name!!!'\n=> Please enter again!!!\n";
 
 //Sending
-char *sending_title1 = "\n------------------------------ Sending ----------------------------\n1.Receiver:";
-char *sending_title2 = "\n2.Files(At most 5 files)\n";
+char *sending_title1 = "\n------------------------------ Sending ----------------------------\nReceiver:";
+char *sending_title2 = "\nPlease input Files' names you want to send(At most 5 files)\n";
 char *sending_err1 = "\n------------------------------ Error ------------------------------\n*Warning:You can not send more than 5 files at the same time!!!\n=> Please enter again!!!\n";
-char *sending_finish = "\n=> Back to home,press:/Home\n";
+char *sending_finish = "\n=> Please input '/Home' if you want back\n";
 
 //Receiving
-char *receiving_title = "\n----------------------------- Receiving ---------------------------\nBack to home,press:/Home\n";
+char *receiving_title = "\n----------------------------- Receiving ---------------------------\nPlease input '/Home' if you want back\n";
